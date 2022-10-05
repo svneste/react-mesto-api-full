083cdf2bcad2957e26cccade085246d58c1e2701
Sergey Nesterovich <blooker@yandex.ru>
2022-10-05 16:39:17 +0300

    add cors

--- backend/app.js
+++ backend/app.js
@@ -19,21 +19,14 @@ const {
   validationLoginUser,
 } = require('./middlewares/validations');
 
-const allowedCors = [
-  'https://facebook.nomoredomains.icu',
-  'localhost:3000',
-];
-
 const app = express();
 app.use(bodyParser.json());
 
 app.use(requestLogger);
 
 app.use(function(req, res, next) {
-
-    res.header('Access-Control-Allow-Origin', '*');
-
-});
+  res.header('Access-Control-Allow-Origin', "*");
+})
 
 app.post('/signin', validationLoginUser, login);
 app.post('/signup', validationCreateUser, createUser);
