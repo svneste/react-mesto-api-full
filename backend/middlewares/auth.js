779c2ba17fcb31ce88d08bfca47b476cd811a315
Sergey Nesterovich <blooker@yandex.ru>
2022-10-12 20:55:00 +0300

    add .env

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized-err');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
@@ -9,7 +11,7 @@ module.exports = (req, res, next) => {
     const token = authorization.replace('Bearer ', '');
     let payload;
     try {
-      payload = jwt.verify(token, 'some-secret-key');
+      payload = jwt.verify(token, `${NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key'}`);
     } catch (err) {
       next(new UnauthorizedError(err.message));
     }
