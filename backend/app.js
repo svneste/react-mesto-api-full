8092504505288297bb35d4258cc464b342b0989f
Sergey Nesterovich <blooker@yandex.ru>
2022-10-12 21:21:28 +0300

    add crash test

--- backend/app.js
+++ backend/app.js
@@ -29,6 +29,12 @@ app.use(cors());
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', validationLoginUser, login);
 app.post('/signup', validationCreateUser, createUser);
 
